3d0a46c6d6e901b77c74fb6c2aff554b92423f8c
Aaron Ecay <aaronecay@gmail.com>
2017-05-08 02:18:54 +0100

    migrate to app.R

--- w1/app.R
+++ w1/app.R
@@ -55,4 +55,49 @@ ui <- fluidPage(titlePanel("Mixed effects workshop demos"),
                              exercise2,
                              exercise3,
                              exercise4))
-ui
+
+server <- function(input, output) {
+    output$r2 <- renderText({
+        if (length(input$predictors) == 0) {
+            "No predictors selected"
+        } else {
+            summary(lm(as.formula(paste0("rt~",
+                                         paste(input$predictors, collapse = "+"))),
+                       data = dat))$r.squared
+        }
+    })
+
+    output$model.summary <- renderPrint({
+        if (length(input$predictors2) == 0) {
+            print("No predictors selected")
+        } else {
+            formula <- as.formula(paste0("rt~",
+                                         paste(input$predictors2, collapse = "+")))
+            exp <- substitute(lm(formula, data = dat),
+                              list(formula = formula))
+            print(summary(eval(exp)))
+        }
+    })
+
+    output$model.summary3 <- renderPrint({
+        if (length(input$predictors3) == 0) {
+            print("No predictors selected")
+        } else {
+            formula <- as.formula(paste0("rt~",
+                                         paste(input$predictors3, collapse = "+")))
+            exp <- substitute(lm(formula, data = dat),
+                              list(formula = formula))
+            print(summary(eval(exp)))
+        }
+    })
+
+    output$ln.summary <- renderPrint({
+        if (input$logp) {
+            print(summary(lm(log(rt) ~ nletters + subtlex.frequency + summed.bigram * lexicality + OLD20 + lett.odd + part3, data = dat)))
+        } else {
+            print(summary(lm(rt ~ nletters * subtlex.frequency + summed.bigram * lexicality + OLD20 + lett.odd + part3, data = dat)))
+        }
+    })
+}
+
+shinyApp(ui, server)
