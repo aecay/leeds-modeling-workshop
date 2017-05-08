a0e7fe747132a4284826fb0f7d861e89110307fe
Aaron Ecay <aaronecay@gmail.com>
2017-05-08 02:09:26 +0100

    move shiny code around

---
+++ w1/ui.R
@@ -0,0 +1,56 @@
+exercise1 <- tabPanel("Exercise 1",
+                     mainPanel(tags$p("The goal of this exercise is to build a model that maximizes",
+                                      "the R", tags$sup("2"), ".  You are given a dataset of response times",
+                                      "on a lexical decision task, and a number of predictors that might",
+                                      "be relevant to the phenomenon.  Try selecting predictors that give",
+                                      "the best R", tags$sup("2"), "."),
+                               checkboxGroupInput("predictors",
+                                                  "Predictors to include",
+                                                  c("lexicality",
+                                                    "nletters",
+                                                    "subtlex.frequency",
+                                                    "summed.bigram",
+                                                    "OLD20",
+                                                    "lett.odd",
+                                                    "part3")),
+                               tags$p("The R", tags$sup("2"), "for this model is: ",
+                                      tags$b(textOutput("r2", inline = T)))))
+
+exercise2 <- tabPanel("Exercise 2",
+                      mainPanel(tags$p("Choose the predictor(s) you wish to see in the model:"),
+                                checkboxGroupInput("predictors2",
+                                                   "Predictors to include",
+                                                   c("lexicality",
+                                                     "nletters",
+                                                     "subtlex.frequency",
+                                                     "summed.bigram",
+                                                     "OLD20",
+                                                     "lett.odd",
+                                                     "part3")),
+                                tags$pre(textOutput("model.summary"))))
+
+exercise3 <- tabPanel("Exercise 3",
+                      mainPanel(tags$p("Choose the predictor(s) you wish to see in the model:"),
+                                checkboxGroupInput("predictors3",
+                                                   "Predictors to include",
+                                                   c("lexicality",
+                                                     "nletters",
+                                                     "subtlex.frequency",
+                                                     "summed.bigram",
+                                                     "OLD20",
+                                                     "lett.odd",
+                                                     "part3",
+                                                     "bigram/lexicality interaction" = "summed.bigram*lexicality" )),
+                                tags$pre(textOutput("model.summary3"))))
+
+exercise4 <- tabPanel("Exercise 4",
+                      mainPanel(checkboxInput("logp", "Take logarithm of response time?"),
+                                tags$pre(textOutput("ln.summary"))))
+
+ui <- fluidPage(titlePanel("Mixed effects workshop demos"),
+                navlistPanel("Example",
+                             exercise1,
+                             exercise2,
+                             exercise3,
+                             exercise4))
+ui
