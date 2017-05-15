b65a15d93f5b52c2bfa3ff1fcda3367e6a4f726d
Aaron Ecay <aaronecay@gmail.com>
2017-05-15 12:55:31 +0100

    Update script.R

--- w2/script.R
+++ w2/script.R
@@ -4,5 +4,6 @@ pacman::p_load(shiny)
 dat <- read.csv("https://github.com/aecay/leeds-modeling-workshop/raw/master/w1/data.csv")
 dat$part3 <- factor(dat$part3)
 dat$lett.odd <- factor(dat$lett.odd)
+dat$participant <- factor(dat$participant)
 
 runGitHub("aecay/leeds-modeling-workshop", subdir="w2")
